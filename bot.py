439706bd8b8d70f4c51858b21fe596cbf84206fd
⌯ Ꭺɴᴏɴʏᴍᴏᴜꜱ | ×͜× | <155171427+NBBotz@users.noreply.github.com>
2025-12-26 00:07:29 +0530

    Fixed: Commands Not Working In Pyrofork v2.3.69
    This update fixes the issue where some commands were being ignored or stopped working after the first plugin was loaded. All plugin files now respond correctly.

--- bot.py
+++ bot.py
@@ -1,8 +1,5 @@
 import plugins.monkey_patch
 import sys
-import glob
-import importlib
-from pathlib import Path
 from pyrogram import Client, idle, __version__
 from pyrogram.raw.all import layer
 import time
@@ -44,17 +41,11 @@ async def dreamxbotz_start():
     bot_info = await dreamxbotz.get_me()
     dreamxbotz.username = bot_info.username
     await initialize_clients()
-    for name in files:
-        with open(name) as a:
-            patt = Path(a.name)
-            plugin_name = patt.stem.replace(".py", "")
-            plugins_dir = Path(f"plugins/{plugin_name}.py")
-            import_path = "plugins.{}".format(plugin_name)
-            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
-            load = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(load)
-            sys.modules["plugins." + plugin_name] = load
-            print("DreamxBotz Imported => " + plugin_name)
+    if 0 in dreamxbotz.dispatcher.groups:
+        all_handlers = list(dreamxbotz.dispatcher.groups[0])
+        for i, handler in enumerate(all_handlers):
+            dreamxbotz.dispatcher.remove_handler(handler, group=0)
+            dreamxbotz.dispatcher.add_handler(handler, group=i)
     if ON_HEROKU:
         asyncio.create_task(ping_server()) 
     b_users, b_chats = await db.get_banned()
